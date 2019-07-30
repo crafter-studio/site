c65125bfb1ca56bdf37b69c05530438f774fac9f
Leroy Wan <leroywan@gmail.com>
2019-07-30 02:05:40 -0400

    Add Waypoint and dark mode

--- src/components/Scroll/Scroll.tsx
+++ src/components/Scroll/Scroll.tsx
@@ -2,58 +2,59 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {ReduxState} from '../../redux/reducers';
 import {toggleDarkMode} from '../../redux/actions';
+import {Waypoint} from 'react-waypoint';
 
 interface Props {
   darkMode?: boolean;
-  handleInView?: () => void;
+  onEnter?: () => void;
+  onLeave?: () => void;
 }
 
 type State = {};
 type ComposedProps = ReduxState & Props;
 
 class Scroll extends React.PureComponent<ComposedProps, State> {
-  private scrollRef: React.RefObject<HTMLInputElement>;
   constructor(props) {
     super(props);
-    this.scrollRef = React.createRef();
-    this.handleScroll = this.handleScroll.bind(this);
+    this.handleOnEnter = this.handleOnEnter.bind(this);
+    this.handleOnLeave = this.handleOnLeave.bind(this);
   }
 
-  componentDidMount() {
-    document.addEventListener('scroll', this.handleScroll);
-  }
+  private handleOnEnter() {
+    const {darkMode, onEnter, dispatch} = this.props;
 
-  componentWillUnmount() {
-    document.removeEventListener('scroll', this.handleScroll);
-  }
-
-  private isInViewport = (e, {top: t, height: h} = e.getBoundingClientRect()) =>
-    t <= innerHeight && t + h >= 0;
-
-  private isAtTopOfPage = (e, {top: t} = e.getBoundingClientRect()) =>
-    t < 0 && t > -100;
-
-  private handleScroll() {
-    const {darkMode, handleInView, dispatch} = this.props;
-
-    if (this.isAtTopOfPage(this.scrollRef.current)) {
-      if (darkMode) {
-        dispatch(toggleDarkMode(true));
-      } else {
-        dispatch(toggleDarkMode(false));
-      }
+    if (darkMode) {
+      dispatch(toggleDarkMode(true));
     }
 
-    if (this.isInViewport(this.scrollRef.current)) {
-      if (handleInView == null) {
-        return;
-      }
-      handleInView();
+    if (onEnter != null) {
+      onEnter();
+    }
+  }
+
+  private handleOnLeave() {
+    const {darkMode, onLeave, dispatch} = this.props;
+
+    if (darkMode) {
+      dispatch(toggleDarkMode(false));
+    }
+
+    if (onLeave != null) {
+      onLeave();
     }
   }
 
   render() {
-    return <div ref={this.scrollRef}>{this.props.children}</div>;
+    return (
+      <Waypoint
+        scrollableAncestor={window}
+        bottomOffset="90%"
+        onEnter={this.handleOnEnter}
+        onLeave={this.handleOnLeave}
+      >
+        {this.props.children}
+      </Waypoint>
+    );
   }
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,31 +20,23 @@ export default class IndexPage extends React.PureComponent<
         keywords={['keyword', 'things']}
       >
         <Layout>
-          <Scroll>
-            <div className={styles.HeroContainer}>
-              <Grid>
-                <Grid.ScreenWidth>
-                  <Text tag="h1">
-                    <div>The best web</div>
-                    <div>studio on</div>
-                    <div>planet Earth.</div>
-                  </Text>
-                </Grid.ScreenWidth>
-              </Grid>
-            </div>
-          </Scroll>
+          <div className={styles.HeroContainer}>
+            <Grid>
+              <Grid.ScreenWidth>
+                <Text tag="h1">
+                  <div>The best web</div>
+                  <div>studio on</div>
+                  <div>planet Earth.</div>
+                </Text>
+              </Grid.ScreenWidth>
+            </Grid>
+          </div>
           <Scroll darkMode>
             <div className={styles.FirstSection}>
               <div className={styles.Hypnotize}>
                 <h1>CRAFTER STUDIO</h1>
               </div>
             </div>
-          </Scroll>
-          <Scroll>
-            <div style={{height: '1000px', background: 'grey'}} />
-          </Scroll>
-          <Scroll darkMode>
-            <div style={{height: '1000px', background: 'yellow'}} />
           </Scroll>
         </Layout>
       </PageWrapper>
