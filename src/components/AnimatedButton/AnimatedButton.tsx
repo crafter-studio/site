be777ae46621861342093e8d85b0b4a69e649b50
Leroy Wan <leroywan@gmail.com>
2019-08-10 21:30:29 -0400

    Enforce fixed widths for animated buttons

--- src/components/AnimatedButton/AnimatedButton.tsx
+++ src/components/AnimatedButton/AnimatedButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'gatsby';
 
+import {classNames} from '../utils/classNames';
 import styles from './AnimatedButton.module.scss';
 
 interface Props {
@@ -19,60 +20,28 @@ export default class AnimatedButton extends React.PureComponent<
   State
 > {
   private ref;
-  private linkRef;
   constructor(props) {
     super(props);
     this.ref = React.createRef();
-    this.linkRef = React.createRef();
   }
-
-  state: State = {
-    renderedWidth: null,
-  };
 
   componentDidMount() {
-    setTimeout(() => {
-      const width = this.linkRef.current.offsetWidth;
-      this.setState({
-        renderedWidth: width,
-      });
-    }, 500);
-  }
+    const {width = 140} = this.props;
 
-  componentDidUpdate() {
-    const {renderedWidth} = this.state;
-    this.ref.current.style.setProperty('--button-width', `${renderedWidth}px`);
+    this.ref.current.style.setProperty('--button-width', `${width}px`);
   }
 
   render() {
-    const {to, content} = this.props;
-    const {renderedWidth} = this.state;
+    const {to, content, width = 140} = this.props;
 
-    const placeholder = (
-      <Link ref={this.linkRef} className={styles.Link} to={to}>
-        {content}
-      </Link>
-    );
-
-    const buttonMarkup = renderedWidth ? (
-      <>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox={`0 0 ${renderedWidth} 40`}
-        >
-          <rect className={styles.Rect} width={renderedWidth} height={40} />
+    return (
+      <div ref={this.ref} className={styles.SvgWrapper}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${width} 40`}>
+          <rect className={styles.Rect} width={width} height={40} />
         </svg>
         <Link className={styles.Link} to={to}>
           {content}
         </Link>
-      </>
-    ) : (
-      placeholder
-    );
-
-    return (
-      <div ref={this.ref} className={styles.SvgWrapper}>
-        {buttonMarkup}
       </div>
     );
   }
