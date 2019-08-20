1c0d3684097fb7cb8f454e264d10e849748cea6d
Leroy Wan <leroywan@gmail.com>
2019-08-20 14:35:06 -0400

    Add cuid to node elements in Article

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactHtmlParser, {processNodes} from 'react-html-parser';
+import cuid from 'cuid';
 
 import styles from './Article.module.scss';
 
@@ -11,38 +12,62 @@ const options = {
 
 function transform(node, index) {
   if (node.type === 'tag' && node.name === 'p') {
-    return <Text>{processNodes(node.children, transform)}</Text>;
+    return <Text key={cuid()}>{processNodes(node.children, transform)}</Text>;
   }
 
   if (node.type === 'tag' && node.name === 'h1') {
-    return <Text tag="h1">{processNodes(node.children, transform)}</Text>;
+    return (
+      <Text key={cuid()} tag="h1">
+        {processNodes(node.children, transform)}
+      </Text>
+    );
   }
 
   if (node.type === 'tag' && node.name === 'h2') {
-    return <Text tag="h2">{processNodes(node.children, transform)}</Text>;
+    return (
+      <Text key={cuid()} tag="h2">
+        {processNodes(node.children, transform)}
+      </Text>
+    );
   }
 
   if (node.type === 'tag' && node.name === 'h3') {
-    return <Text tag="h3">{processNodes(node.children, transform)}</Text>;
+    return (
+      <Text key={cuid()} tag="h3">
+        {processNodes(node.children, transform)}
+      </Text>
+    );
   }
 
   if (node.type === 'tag' && node.name === 'h4') {
-    return <Text tag="h4">{processNodes(node.children, transform)}</Text>;
+    return (
+      <Text key={cuid()} tag="h4">
+        {processNodes(node.children, transform)}
+      </Text>
+    );
   }
 
   if (node.type === 'tag' && node.name === 'h5') {
-    return <Text tag="h5">{processNodes(node.children, transform)}</Text>;
+    return (
+      <Text key={cuid()} tag="h5">
+        {processNodes(node.children, transform)}
+      </Text>
+    );
   }
 
   if (node.type === 'tag' && node.name === 'h6') {
-    return <Text tag="h6">{processNodes(node.children, transform)}</Text>;
+    return (
+      <Text key={cuid()} tag="h6">
+        {processNodes(node.children, transform)}
+      </Text>
+    );
   }
 
   if (node.type == 'tag' && node.attribs.class != null) {
     const {class: className} = node.attribs;
     if (className.includes('full-width')) {
       return (
-        <Article.FullWidth>
+        <Article.FullWidth key={cuid()}>
           {processNodes(node.children, transform)}
         </Article.FullWidth>
       );
@@ -50,7 +75,9 @@ function transform(node, index) {
 
     if (className.includes('wide')) {
       return (
-        <Article.Wide>{processNodes(node.children, transform)}</Article.Wide>
+        <Article.Wide key={cuid()}>
+          {processNodes(node.children, transform)}
+        </Article.Wide>
       );
     }
   }
