3ca2eeb0de341da140b692f48c14f89075def26d
Leroy Wan <leroywan@gmail.com>
2019-08-21 23:40:20 -0400

    Add placeholder Work section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -12,6 +12,11 @@ import {
 } from '../../components';
 
 import placeholder from '../../images/casper.png';
+import placeholder_1 from '../../images/placeholder_1.jpg';
+import placeholder_2 from '../../images/placeholder_2.jpg';
+import placeholder_3 from '../../images/placeholder_3.jpg';
+import placeholder_4 from '../../images/placeholder_4.jpg';
+import placeholder_5 from '../../images/placeholder_5.jpg';
 
 interface Props {}
 
@@ -81,6 +86,34 @@ export default class Home extends React.PureComponent<ComposedProps, State> {
               </Grid.ScreenWidth>
             </Grid>
           </div>
+          <Page.Section bgColor="var(--color-light-grey)">
+            <Grid>
+              <Grid.ScreenWidth>
+                <div className={styles.WorkSection}>
+                  <div className={styles.Heading}>
+                    <h1>Work.</h1>
+                  </div>
+                  <div className={styles.WorkGrid}>
+                    <div className={styles.Big}>
+                      {/* <img src={placeholder_2} /> */}
+                    </div>
+                    <div className={styles.B}>
+                      {/* <img src={placeholder_5} /> */}
+                    </div>
+                    <div className={styles.C}>
+                      {/* <img src={placeholder_1} /> */}
+                    </div>
+                    <div className={styles.D}>
+                      {/* <img src={placeholder_4} /> */}
+                    </div>
+                    <div className={styles.E}>
+                      {/* <img src={placeholder_3} /> */}
+                    </div>
+                  </div>
+                </div>
+              </Grid.ScreenWidth>
+            </Grid>
+          </Page.Section>
           <Page.Section bgColor="var(--color-dark-grey)">
             <Grid>
               <Text size="h1" align="center" color="var(--color-heading)">
