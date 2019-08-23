4d7a56a59f27b9f4b370498255523ed42f50cd62
Leroy Wan <leroywan@gmail.com>
2019-08-23 12:52:34 -0400

    Remove old code in Blog

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,25 +1,6 @@
 import React from 'react';
-import {StaticQuery, graphql} from 'gatsby';
 
-import ReactHtmlParser, {
-  processNodes,
-  convertNodeToElement,
-  htmlparser2,
-} from 'react-html-parser';
-
-import {Page, Grid, Text} from '../../components';
-
-export const query = graphql`
-  query {
-    allWordpressPost {
-      edges {
-        node {
-          content
-        }
-      }
-    }
-  }
-`;
+import {Page} from '../../components';
 
 interface Props {
   data: any;
@@ -28,32 +9,8 @@ interface Props {
 type State = {};
 type ComposedProps = Props;
 
-const options = {
-  transform,
-};
-
-function transform(node, index) {
-  // do not render any <span> tags
-
-  if (node.type === 'tag' && node.name === 'p') {
-    return <Text>{processNodes(node.children, transform)}</Text>;
-  }
-
-  if (node.type === 'tag' && node.name === 'figure') {
-    return (
-      <Grid.ScreenWidth>
-        <div style={{marginTop: '40px', marginBottom: '40px'}}>
-          {processNodes(node.children, transform)}
-        </div>
-      </Grid.ScreenWidth>
-    );
-  }
-}
-
-class Blog extends React.PureComponent<ComposedProps, State> {
+export default class Blog extends React.PureComponent<ComposedProps, State> {
   render() {
-    const html = this.props.data.allWordpressPost.edges[0].node.content;
-    const markup = ReactHtmlParser(html, options);
     return (
       <Page
         title="Blog"
@@ -61,14 +18,9 @@ class Blog extends React.PureComponent<ComposedProps, State> {
         keywords={['keyword', 'things']}
       >
         <Page.Layout>
-          <div style={{height: '200px'}} />
-          <Grid>{markup}</Grid>
+          <h1>Blog</h1>
         </Page.Layout>
       </Page>
     );
   }
 }
-
-export default () => (
-  <StaticQuery query={query} render={(data) => <Blog data={data} />} />
-);
