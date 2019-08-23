8dd13bd81dbef99e85bc5550e1464e16a2bb2085
Leroy Wan <leroywan@gmail.com>
2019-08-23 13:01:02 -0400

    Fix footer spacing

--- src/components/Page/components/Layout/components/Footer/Footer.tsx
+++ src/components/Page/components/Layout/components/Footer/Footer.tsx
@@ -19,97 +19,90 @@ export default class Footer extends React.PureComponent<ComposedProps, State> {
   render() {
     return (
       <footer className={styles.Footer}>
-        <Page.Section>
-          <Grid>
-            <Grid.ScreenWidth>
-              <div className={styles.FooterGrid}>
-                <div className={styles.FooterItem}>
-                  <Text tag="h4" size="small" uppercase>
-                    Random
+        <Grid>
+          <Grid.ScreenWidth>
+            <div className={styles.FooterGrid}>
+              <div className={styles.FooterItem}>
+                <Text tag="h4" size="small" uppercase>
+                  Random
+                </Text>
+                <Text.Container>
+                  <Text size="small">
+                    Don't be an apple, eat cheese. Why haven't everyone figured
+                    out why children like to roll in mud so much? I think it is
+                    because in their past life they were little pigs. Don't be
+                    an apple, eat cheese. Why haven't everyone figured out why
+                    children like to roll in mud so much?
                   </Text>
-                  <Text.Container>
-                    <Text size="small">
-                      Don't be an apple, eat cheese. Why haven't everyone
-                      figured out why children like to roll in mud so much? I
-                      think it is because in their past life they were little
-                      pigs. Don't be an apple, eat cheese. Why haven't everyone
-                      figured out why children like to roll in mud so much?
-                    </Text>
-                  </Text.Container>
-                </div>
-                <div className={styles.FooterItem}>
-                  <Text tag="h4" size="small" uppercase>
-                    Blog
-                  </Text>
-                  <List noPadding noBullets>
-                    <List.Item>
-                      <Text size="small">
-                        <Link to="blog">
-                          A longer post featuring something cool. This is a
-                          title.
-                        </Link>
-                      </Text>
-                    </List.Item>
-                    <List.Item>
-                      <Text size="small">
-                        <Link to="blog">
-                          Why are web developers so overpaid?
-                        </Link>
-                      </Text>
-                    </List.Item>
-                    <List.Item>
-                      <Text size="small">
-                        <Link to="blog">Web design trends of 2020</Link>
-                      </Text>
-                    </List.Item>
-                    <List.Item>
-                      <Text size="small">
-                        <Link to="blog">
-                          Life is too short, eat more veggies.
-                        </Link>
-                      </Text>
-                    </List.Item>
-                    <List.Item>
-                      <Text size="small">
-                        <Link to="blog">One More blog post here.</Link>
-                      </Text>
-                    </List.Item>
-                  </List>
-                </div>
-                <div className={styles.FooterItem}>
-                  <Text tag="h4" size="small" uppercase>
-                    Contact Us
-                  </Text>
-                  <List noPadding noBullets>
-                    <List.Item>
-                      <Text size="small">23 Rue Jernof Street, M2M 6Z9</Text>
-                    </List.Item>
-                    <List.Item>
-                      <Text size="small">Toronto, Ontario</Text>
-                    </List.Item>
-                    <List.Item>
-                      <Text size="small">+1 905 135 9425</Text>
-                    </List.Item>
-                    <List.Item>
-                      <Text size="small">hello@crafterstu.com</Text>
-                    </List.Item>
-                    <List.Item>
-                      <Button value="Contact Us" linkPath="/contact" />
-                    </List.Item>
-                  </List>
-                </div>
+                </Text.Container>
               </div>
-            </Grid.ScreenWidth>
-          </Grid>
-        </Page.Section>
+              <div className={styles.FooterItem}>
+                <Text tag="h4" size="small" uppercase>
+                  Blog
+                </Text>
+                <List noPadding noBullets>
+                  <List.Item>
+                    <Text size="small">
+                      <Link to="blog">
+                        A longer post featuring something cool. This is a title.
+                      </Link>
+                    </Text>
+                  </List.Item>
+                  <List.Item>
+                    <Text size="small">
+                      <Link to="blog">Why are web developers so overpaid?</Link>
+                    </Text>
+                  </List.Item>
+                  <List.Item>
+                    <Text size="small">
+                      <Link to="blog">Web design trends of 2020</Link>
+                    </Text>
+                  </List.Item>
+                  <List.Item>
+                    <Text size="small">
+                      <Link to="blog">
+                        Life is too short, eat more veggies.
+                      </Link>
+                    </Text>
+                  </List.Item>
+                  <List.Item>
+                    <Text size="small">
+                      <Link to="blog">One More blog post here.</Link>
+                    </Text>
+                  </List.Item>
+                </List>
+              </div>
+              <div className={styles.FooterItem}>
+                <Text tag="h4" size="small" uppercase>
+                  Contact Us
+                </Text>
+                <List noPadding noBullets>
+                  <List.Item>
+                    <Text size="small">23 Rue Jernof Street, M2M 6Z9</Text>
+                  </List.Item>
+                  <List.Item>
+                    <Text size="small">Toronto, Ontario</Text>
+                  </List.Item>
+                  <List.Item>
+                    <Text size="small">+1 905 135 9425</Text>
+                  </List.Item>
+                  <List.Item>
+                    <Text size="small">hello@crafterstu.com</Text>
+                  </List.Item>
+                  <List.Item>
+                    <Button value="Contact Us" linkPath="/contact" />
+                  </List.Item>
+                </List>
+              </div>
+            </div>
+          </Grid.ScreenWidth>
+        </Grid>
         <div className={styles.Copyright}>
           <Grid>
             <Grid.ScreenWidth>
-              <Text.Container align="center">
-                <Text size="subscript">
-                  Copyright © {new Date().getFullYear()} Crafter Studio
-                </Text>
-              </Text.Container>
+              <Text size="subscript">
+                Copyright © {new Date().getFullYear()} Crafter Studio
+              </Text>
             </Grid.ScreenWidth>
           </Grid>
         </div>
