6ef47364e72fdce83e4e1c341add395c9c7d2a9f
Leroy Wan <leroywan@gmail.com>
2019-08-28 15:44:54 -0400

    Add projects in Work

--- src/pages/work/Work.tsx
+++ src/pages/work/Work.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {StaticQuery, graphql} from 'gatsby';
+import striptags from 'striptags';
 
 import styles from './Work.module.scss';
-import {Page} from '../../components';
-
+import {Page, Grid, Text, List, Link} from '../../components';
 import {nodeFromEdges} from '../../components/utils';
+
+import placeholder from '../../images/placeholder.jpg';
 
 interface Props {
   data: any;
@@ -12,6 +14,24 @@ interface Props {
 
 type State = {};
 type ComposedProps = Props;
+
+const Project = ({slug, title, excerpt, tags}) => {
+  return (
+    <div className={styles.Project}>
+      <img src={placeholder} />
+      <h2 className={styles.ProjectTitle}>{title}</h2>
+      <Text>{striptags(excerpt)}</Text>
+      <List>
+        {tags.map((tag, key) => (
+          <List.Item key={key}>{tag.name}</List.Item>
+        ))}
+      </List>
+      <Link to={`/work/${slug}`} animated>
+        Slug: {slug}
+      </Link>
+    </div>
+  );
+};
 
 class Work extends React.PureComponent<ComposedProps, State> {
   state: State = {};
@@ -22,6 +42,7 @@ class Work extends React.PureComponent<ComposedProps, State> {
   render() {
     const {data} = this.props;
     const projects = nodeFromEdges(data.allWordpressWpProject.edges);
+
     return (
       <Page
         title="Web Design and Development"
@@ -31,9 +52,21 @@ class Work extends React.PureComponent<ComposedProps, State> {
         <Page.Background />
         <Page.Layout>
           <Page.Section>
-            {projects.map((item, key) => (
-              <h1 key={key}>{`${item.title}`}</h1>
-            ))}
+            <Grid>
+              <Grid.ScreenWidth>
+                <div className={styles.ProjectsContainer}>
+                  {projects.map((project, key) => (
+                    <Project
+                      title={project.title}
+                      slug={project.slug}
+                      tags={project.tags}
+                      excerpt={project.excerpt}
+                      key={key}
+                    />
+                  ))}
+                </div>
+              </Grid.ScreenWidth>
+            </Grid>
           </Page.Section>
         </Page.Layout>
       </Page>
@@ -51,6 +84,10 @@ export default () => (
               slug
               title
               excerpt
+              tags {
+                id
+                name
+              }
               acf {
                 test_label_1
                 test_label_2
