c2729646b440756529189b4f7c9835c1361ece70
Leroy Wan <leroywan@gmail.com>
2019-09-02 18:58:54 -0400

    Add Contact page

---
+++ src/pages/contact/Contact.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styles from './Contact.module.scss';
+
+import {Page, Grid} from '../../components';
+
+interface Props {}
+interface State {}
+
+type ComposedProps = Props;
+
+export default class Contact extends React.PureComponent<ComposedProps, State> {
+  render() {
+    return (
+      <Page
+        title="Web Design and Development"
+        description="Description"
+        keywords={['keyword', 'things']}
+      >
+        <Page.Background />
+        <Page.Layout>
+          <Grid>
+            <form name="contact" method="POST" data-netlify="true">
+              <p>
+                <label>
+                  Your Name: <input type="text" name="name" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Email: <input type="email" name="email" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Role:{' '}
+                  <select name="role[]" multiple>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                  </select>
+                </label>
+              </p>
+              <p>
+                <label>
+                  Message: <textarea name="message" />
+                </label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
+          </Grid>
+        </Page.Layout>
+      </Page>
+    );
+  }
+}
