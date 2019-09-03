e89859e515979857265aa026baa84e7f2d0353c0
Leroy Wan <leroywan@gmail.com>
2019-09-02 20:11:32 -0400

    Edit contact form

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -19,40 +19,17 @@ export default class Contact extends React.PureComponent<ComposedProps, State> {
         <Page.Background />
         <Page.Layout>
           <Grid>
-            <form
-              name="contact"
-              method="post"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-            >
-              <input type="hidden" name="crafter-studio-form" value="contact" />
-              <p>
-                <label>
-                  Your Name: <input type="text" name="name" />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Your Email: <input type="email" name="email" />
-                </label>
-              </p>
-              <p>
-                <label>
-                  Your Role:{' '}
-                  <select name="role[]" multiple>
-                    <option value="leader">Leader</option>
-                    <option value="follower">Follower</option>
-                  </select>
-                </label>
-              </p>
-              <p>
-                <label>
-                  Message: <textarea name="message" />
-                </label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
+            <form name="Contact Form" method="POST" data-netlify="true">
+              <input type="hidden" name="form-name" value="Contact Form" />
+              <div>
+                <label>Your Email:</label>
+                <input type="email" name="email" />
+              </div>
+              <div>
+                <label>Message:</label>
+                <textarea name="message" />
+              </div>
+              <button type="submit">Send</button>
             </form>
           </Grid>
         </Page.Layout>
