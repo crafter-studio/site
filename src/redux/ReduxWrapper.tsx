eec9c49b5bbd1b33023fc3742d6aedffffeea70f
Leroy Wan <leroywan@gmail.com>
2019-10-10 19:42:23 -0500

    Disable redux logger

--- src/redux/ReduxWrapper.tsx
+++ src/redux/ReduxWrapper.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {createStore as reduxCreateStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import rootReducer from '.';
-const createStore = () =>
-  reduxCreateStore(rootReducer, applyMiddleware(logger));
+// const createStore = () =>
+//   reduxCreateStore(rootReducer, applyMiddleware(logger));
+
+const createStore = () => reduxCreateStore(rootReducer);
 export default ({element}) => (
   <Provider store={createStore()}>{element}</Provider>
 );
