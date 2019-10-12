bc18a4b29f8e641629c0c89e14a091025d755048
Leroy Wan <leroywan@gmail.com>
2019-10-12 16:02:07 -0500

    Move background inside Layout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -156,8 +156,8 @@ class Home extends React.PureComponent<ComposedProps, State> {
 
     return (
       <Page title="Web Design and Development">
-        <Page.Background blob />
         <Page.Layout>
+        <Page.Background blob />
           {/* ================================================================================================================= */}
           {/* HERO SECTION */}
           {/* ================================================================================================================= */}
